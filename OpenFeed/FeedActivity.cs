7f8f31651823a70dd4b488d8b9e4cd52e1f5b4d5
thandon263 <ncubethando20@gmail.com>
2016-08-24 00:23:10 -0400

    Android using Xamarin studio C# application

---
+++ OpenFeed/FeedActivity.cs
@@ -0,0 +1,28 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace OpenFeed
+{
+	[Activity(Label = "OpenFeed", MainLauncher = true, Icon = "@drawable/icon")]
+	public class FeedActivity : ListActivity
+	{
+		private RssItem[] _items;
+
+		protected override void OnCreate(Bundle bundle)
+		{
+			base.OnCreate(bundle);
+
+			using (var client = new HttpClient())
+			{
+				var xmlFeed = await client.GetStringAsync("http://blog.xamarin.com/feed/");
+				var doc = XDocument.Parse(xmlFeed);
+				XNamespace dc = "http:/purl.org/dc/element/1.1/";
+
+				_items =
+			}
+		}
+	}
+}
+
+
